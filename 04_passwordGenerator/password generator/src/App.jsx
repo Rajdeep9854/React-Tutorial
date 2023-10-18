e65ff22a0ba93a406d0b607dce053113bf84936a
Rajdeep <rajdeep98543231@gmail.com>
2023-10-18 20:33:43 +0530

    passwod generator

---
+++ 04_passwordGenerator/password generator/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='h-screen w-full bg-black'>
+        <h1 className='text-4xl text-center text-yellow-300 '>Password Generator</h1>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
