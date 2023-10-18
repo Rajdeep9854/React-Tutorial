77514d83567818a0653a0694ed01d2562f7ba21a
Rajdeep <rajdeep98543231@gmail.com>
2023-10-18 21:27:59 +0530

    sdvasd

--- 04_passwordGenerator/password generator/src/App.jsx
+++ 04_passwordGenerator/password generator/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
     <div className='h-screen w-full bg-black'>
-        <h1 className='text-4xl text-center text-yellow-300 '>Password Generator</h1>
+        <h1 className='text-4xl text-center text-yellow-300 '>My Todos</h1>
     </div>
       
     </>
